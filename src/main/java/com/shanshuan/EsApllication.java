2953ce6ddd10a1ee481a21a1a038a1393fe041fc
wangzifeng <xiaobai@yscredit.com>
2019-12-20 11:34:19 +0800

    3333333333

--- src/main/java/com/shanshuan/EsApllication.java
+++ src/main/java/com/shanshuan/EsApllication.java
@@ -10,6 +10,7 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class EsApllication {
     public static void main(String[] args) {
+        System.out.println("我是错误版本");
         SpringApplication.run(EsApllication.class);
     }
 }
